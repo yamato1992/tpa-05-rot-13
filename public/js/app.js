25f1dbd023e7377153f6d1268d5784a0901c0f23
yamato1992 <yamato.mitsui.orleans@gmail.com>
2019-01-22 22:23:16 +0900

    clean app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,38 @@
-const rot13Input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
-const rot13Output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'.split('');
+const Rot13 = (function() {
 
-const rotateChar = function(ch) {
-  return rot13Input.indexOf(ch) > -1 ? rot13Output[rot13Input.indexOf(ch)] : ch;
-};
+    const DOMstrings = {
+        sourceText: '#container .source-text',
+        destinationText: '#container .destination-text',
+        formConvert: '#container .destination-text',
+        btnConvert: '#container .btn-convert'
+    };
+    
+    const rot13Input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+    const rot13Output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'.split('');
 
-const convertRot13 = function(sourceText) {
-  return sourceText.split('').map(rotateChar).join('');
-};
+    const rotateChar = function(ch) {
+      return rot13Input.indexOf(ch) > -1 ? rot13Output[rot13Input.indexOf(ch)] : ch;
+    };
 
-const handleBtnConvert = function() {
-  const sourceText = document.querySelector('#container .source-text').value;
-  const convertedText = convertRot13(sourceText);
-  document.querySelector('#container .destination-text').value = convertedText;
-};
+    const convertRot13 = function(sourceText) {
+      return sourceText.split('').map(rotateChar).join('');
+    };
 
-const initPage = function() {
-  const form = document.querySelector('#container .form-convert');
-  // see https://developer.mozilla.org/ja/docs/Web/API/Event/preventDefault
-  form.addEventListener('submit', evt => evt.preventDefault());
+    const handleBtnConvert = function() {
+      const sourceText = document.querySelector(DOMstrings.sourceText).value;
+      const convertedText = convertRot13(sourceText);
+      document.querySelector(DOMstrings.destinationText).value = convertedText;
+    };
 
-  document.querySelector('.btn-convert')
-    .addEventListener('click', handleBtnConvert);
-};
+    const initPage = function() {
+      const form = document.querySelector(DOMstrings.formConvert);
+      // see https://developer.mozilla.org/ja/docs/Web/API/Event/preventDefault
+      form.addEventListener('submit', evt => evt.preventDefault());
 
-initPage();
+      const btn = document.querySelector(DOMstrings.btnConvert);
+      btn.addEventListener('click', handleBtnConvert);
+    };
+
+    initPage();
+
+})();
